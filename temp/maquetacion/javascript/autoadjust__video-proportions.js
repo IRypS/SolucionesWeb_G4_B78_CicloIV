f20479fa6845d6d1f3b84688221ef4c308fa2ef8
IRypS <pelaezricardo1502@gmail.com>
2022-10-19 12:51:04 -0500

    Arreglando el script autoajustar altura - 20221019

--- temp/maquetacion/javascript/autoadjust__video-proportions.js
+++ temp/maquetacion/javascript/autoadjust__video-proportions.js
@@ -3,8 +3,6 @@
 
 ( function() {
 
-    let getContainerWidth = document.querySelector('.c_img-video').clientWidth;
-
     let allVideoContainers = [...document.querySelectorAll('.c_img-video')];
 
     setNewHeigth();
@@ -14,7 +12,10 @@
     }
 
     function setNewHeigth() {
-        let calculateHeigth = (getContainerWidth * 0.5625).toString() + 'px';
+        // Get reference from a first element
+        let width = allVideoContainers[0].clientWidth;
+        
+        let calculateHeigth = (width * 0.5625).toString() + 'px';
 
         allVideoContainers.forEach( img => {
             img.style.height = calculateHeigth;
