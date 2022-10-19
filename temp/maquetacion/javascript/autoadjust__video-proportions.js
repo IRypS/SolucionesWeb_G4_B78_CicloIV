6119180bb6694e6f06467c57ad6901d115fefcd7
IRypS <pelaezricardo1502@gmail.com>
2022-10-18 21:53:40 -0500

    Maquetacion Pagina Trailer Vista - 20221018

---
+++ temp/maquetacion/javascript/autoadjust__video-proportions.js
@@ -0,0 +1,24 @@
+// Funcion autoinvocada
+
+
+( function() {
+
+    let getContainerWidth = document.querySelector('.c_img-video').clientWidth;
+
+    let allVideoContainers = [...document.querySelectorAll('.c_img-video')];
+
+    setNewHeigth();
+
+    window.onresize = () => {
+        setNewHeigth();
+    }
+
+    function setNewHeigth() {
+        let calculateHeigth = (getContainerWidth * 0.5625).toString() + 'px';
+
+        allVideoContainers.forEach( img => {
+            img.style.height = calculateHeigth;
+        });
+    };
+
+}) ();
