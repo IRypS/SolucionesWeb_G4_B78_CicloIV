177dfaacb1d818fc4809d15255410d7a6499d46a
IRypS <pelaezricardo1502@gmail.com>
2022-10-23 21:03:42 -0500

    Action buttons trailer - 20221023

---
+++ src/main/java/com/soluciones/web/appGrupo4/model/UserList.java
@@ -0,0 +1,37 @@
+package com.soluciones.web.appGrupo4.model;
+
+import java.util.List;
+
+public class UserList {
+    
+    private String id;
+    private String userId;
+    private String name;
+    private List<String> trailersId;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<String> getTrailersId() {
+        return trailersId;
+    }
+    public void setTrailersId(List<String> trailersId) {
+        this.trailersId = trailersId;
+    }
+
+}
