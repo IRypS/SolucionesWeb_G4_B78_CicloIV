2afea35a2c33a8240e3dc074e4a8002fb67fdd12
IRypS <pelaezricardo1502@gmail.com>
2022-11-16 10:40:02 -0500

    Finalizando evidencia 3 ~

--- temp/sql/insert_activity.sql
+++
@@ -1,8 +0,0 @@
-
-USE trailersdb;
-
-
-INSERT INTO activity (id_activity, name_activity) 
-	VALUES ('6a819380-bb3b-471e-9aa2-b5374a300189', 'actor'),
-    ('58769771-c7e1-4665-9786-4ce702a2dd35', 'director'), 
-    ('3812fcea-1051-458a-9236-4ab1d1c192e2', 'guionista');
